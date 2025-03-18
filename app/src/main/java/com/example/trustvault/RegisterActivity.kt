0ef9f67066337a13009ef0bf55e3384149d1e7a6
alexsugimoto <alexsugimoto@outlook.com>
2025-03-18 15:21:17 +0100

    build: refactored register activity to com.example.trustvault

--- app/src/main/java/com/example/trustvault/RegisterActivity.kt
+++ app/src/main/java/com/example/trustvault/RegisterActivity.kt
@@ -1,4 +1,4 @@
-package com.example.trustvault.ui.theme
+package com.example.trustvault
 
 
 import androidx.compose.foundation.Image
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,13 +28,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trustvault.R
 
 class RegisterActivity {
     @Composable
