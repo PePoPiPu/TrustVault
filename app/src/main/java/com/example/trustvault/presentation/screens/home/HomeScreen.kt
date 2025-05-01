5e882f7e07284912869c1c9e50776e1c0544dd78
alexsugimoto <alexsugimoto@outlook.com>
2025-05-01 17:17:48 +0200

    UI: created floating button for adding passwords in HomeScreen

--- app/src/main/java/com/example/trustvault/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/trustvault/presentation/screens/home/HomeScreen.kt
@@ -1,21 +1,55 @@
 package com.example.trustvault.presentation.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.trustvault.presentation.theme.DarkColorScheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    onAddClick: () -> Unit = {}
+) {
     Column (
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "HomeScreen")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            FloatingActionButton(
+                onClick = onAddClick,
+                containerColor = DarkColorScheme.background
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    tint = Color.White,
+                    contentDescription = "Add"
+                )
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
